0c0f58e659a191147161c81753ba9be4e505af52
Leonardo Medina <leonarmed@gmail.com>
2022-09-30 01:56:06 +0000

    Todolist Python

---
+++ src/app.py
@@ -0,0 +1,32 @@
+from flask import Flask, jsonify, request
+import json
+
+app = Flask(__name__)
+
+todos = [
+    { "id": 0, "label": "My first task", "done": False },
+    { "id": 1, "label": "My second task", "done": False }
+]
+
+@app.route('/todos', methods=['GET'])
+def hello_world():
+    return jsonify(todos)
+
+@app.route('/todos', methods=['POST'])
+def add_new_todo():
+    decoded_object = json.loads(request.data)
+    todos.append(decoded_object)
+    return jsonify(todos)
+
+@app.route('/todos/<int:position>', methods=['DELETE'])
+def delete_todo(position):
+    print("This is the position to delete: ",position)
+    todos.pop(position)
+    return jsonify(todos)
+
+
+
+# Estas dos líneas siempre seben estar al final de tu archivo app.py.
+
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', port=3245, debug=True)
